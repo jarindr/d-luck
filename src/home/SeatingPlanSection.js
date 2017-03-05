2c41e5a6dda0d78444eb8e5a36a9f1ecd174d86d
jarindr <jarindr23@hotmail.com>
2017-03-06 00:54:07 +0700

    done responsive

--- src/home/SeatingPlanSection.js
+++ src/home/SeatingPlanSection.js
@@ -1,6 +1,49 @@
+import Modal from 'react-modal'
 import React from 'react'
 import styles from './SeatingPlanSection.styl'
-
+const DATASEAT = {
+  ocean: {
+    key: 'ocean',
+    title: 'OCEAN ZONE',
+    image: require('../assets/images/ocean-map.png'),
+    imageSeat: require('../assets/images/ocean-seat.png'),
+    content: `ที่นั่งโซนนี้อยู่ใกล้ชิดเวทีการแสดงมากที่สุด เบาะที่นั่งมีความกว้าง 53 ซม. เหมาะสำหรับผู้ที่ต้องการรับชมการแสดงในระยะใกล้ เห็นรายละเอียดต่างๆ บนเวทีได้อย่างชัดเจน
+      ทำให้คุณได้สัมผัสประสบการณ์การแสดงอย่างใกล้ชิดราวกับได้หลุดเข้าไปในโลกของการแสดงจริงๆ`,
+    price: '3,000',
+    link: 'http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Ocean'
+  },
+  star: {
+    key: 'star',
+    title: 'STAR ZONE',
+    image: require('../assets/images/star-map.png'),
+    imageSeat: require('../assets/images/star.png'),
+    content: `นี่คือโซนพิเศษที่ดีที่สุด (VIP) เบาะที่นั่งมีความกว้างถึง 56 ซม. เป็นเบาะที่นั่งที่กว้างที่สุด ให้ความรู้สึกสะดวกสบายเวลารับชม ที่นั่งโซนนี้อยู่จุดศูนย์กลางโรงละคร
+     ซึ่งเป็นตำแหน่งที่รับชมการแสดงได้ดีที่สุด  ทำให้คุณได้ชมโชว์ด้วยความประทับใจ จากมุมมองที่ยอดเยี่ยม สามารถรับชมภาพฉายบนจอขนาดใหญ่ที่ฉากหลังและพื้น
+     (High Definition Projection Mapping) ได้อย่างชัดเจนที่สุด และยังเป็นจุดที่รับฟังระบบเสียงที่ได้รับการออกแบบอย่างมีประสิทธิภาพ ได้สมบูรณ์แบบที่สุดด้วย`,
+    price: '4,000',
+    link: 'http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Star'
+  },
+  cloud: {
+    key: 'cloud',
+    title: 'CLOUD ZONE',
+    image: require('../assets/images/cloud-map.png'),
+    imageSeat: require('../assets/images/sky-seat.png'),
+    content: `่นั่งโซนนี้อยู่บริเวณศูนย์กลางโรงละคร มีมุมมองที่ใกล้เคียงกับ Star Zone เบาะที่นั่งมีความกว้าง 53 ซม. คุณจะได้ชมโชว์ด้วยความประทับใจในมุมมองที่ดี
+    และเต็มอิ่มกับรายละเอียดต่างๆ บนเวที ทั้งระบบแสง เสียง และเทคนิคพิเศษมากมายอย่างเต็มอรรถรส`,
+    price: '3,000',
+    link: 'http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Cloud'
+  },
+  sky: {
+    key: 'sky',
+    title: 'SKY ZONE',
+    image: require('../assets/images/sky-map.png'),
+    imageSeat: require('../assets/images/purple-seat.png'),
+    content: `่นัที่นั่งโซนราคาคุ้มค่านี้ อยู่ในจุดรับชมการแสดงที่ถัดออกมาจากโซนอื่นๆ เบาะที่นั่งมีความกว้าง 51 ซม. คุณจะรับชมการแสดงแบบองค์รวมได้อย่างครบอรรถรสที่สุด
+    สามารถมองเห็นรายละเอียดการแสดงที่เกิดขึ้นได้ทั่วทั้งหมด พร้อมกันนี้คุณจะได้ตื่นตาตื่นใจกับฉากหลังสุดอลังการไม่แพ้โซนอื่นๆ เช่นกัน`,
+    price: '2,500',
+    link: 'http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Sky'
+  }
+}
 const SeatingPlanSection = React.createClass({
   componentDidMount () {
     require('../assets/js/imageMap')
@@ -14,7 +57,9 @@ const SeatingPlanSection = React.createClass({
   },
   onChangeState (state, click) {
     if (this.state.isClicked && click !== 'click') {
-      return (e) => {}
+      return (e) => {
+        this.setState({isModalOpen: true})
+      }
     } else {
       return (e) => {
         if (click) this.setState({isClicked: true})
@@ -30,9 +75,43 @@ const SeatingPlanSection = React.createClass({
       return this.state.current === state && this.state.isClicked ? {opacity: 1, zIndex: 2} : {opacity: 0, zIndex: 1}
     }
   },
+  renderModalPopUp (x) {
+    return (
+      <Modal
+        isOpen={this.state.current === x.key && this.state.isClicked}
+        onRequestClose={this.closeModal}
+        className={styles.modal}
+      >
+        <div className={styles.modalMapContainer}>
+          <div className={styles.mapContainerPopUp}>
+            <img src={x.image} />
+          </div>
+          <div className={`${styles.contentContainer} ${styles.popup}`}>
+            <img
+              src={require('../assets/images/closeButton.png')}
+              className={styles.closeButton}
+              onClick={() => this.setState({current: 'all'})}
+            />
+            <div className={styles.content}>
+              <h1>SEATING PLAN</h1>
+              <h3><img src={x.imageSeat} /> {x.title}</h3>
+              <p>
+              {x.content}
+              </p>
+              <p>ราคา {x.price} บาท</p>
+              <a target='_blank' href={x.link}>
+                <button type='button'>BUY TICKET</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    )
+  },
   render () {
     return (
       <div className={styles.container}>
+        {Object.keys(DATASEAT).map(x => this.renderModalPopUp(DATASEAT[x]))}
         <h1 className={styles.mobileHeader}>SEATING PLAN</h1>
         <div className={styles.mapContainer}>
           <img src={require('../assets/images/sky-map-hover.png')} style={{opacity: this.state.current === 'sky' ? 1 : 0}} />
@@ -45,7 +124,7 @@ const SeatingPlanSection = React.createClass({
               useMap="#Map"
               style={{
                 opacity: this.state.current === 'all' ? 1 : 0,
-                zIndex: 100
+                zIndex: 10
               }}
               onMouseOut={this.onChangeState('all')}
             />
@@ -76,7 +155,7 @@ const SeatingPlanSection = React.createClass({
           </map>
         </div>
         <div className={styles.contentContainer} onMouseEnter={this.onChangeState('all')}>
-          <div className={`${styles.content} ${styles.default}`} style={this.getStyleForCurrent('all')}>
+          <div className={`${styles.content} ${styles.default}`}>
             <h1>SEATING PLAN</h1>
             <h2>ประเภทของที่นั่ง</h2>
             <p>
@@ -101,59 +180,6 @@ const SeatingPlanSection = React.createClass({
               <img src={require('../assets/images/star.png')} />
               Star Zone
             </div>
-          </div>
-          {/* -----------------------------------------------------------------*/}
-          <div className={styles.content} style={this.getStyleForCurrent('star')}>
-            <h1>SEATING PLAN</h1>
-            <h3><img src={require('../assets/images/star.png')} /> STAR ZONE</h3>
-            <p>
-              นี่คือโซนพิเศษที่ดีที่สุด (VIP) เบาะที่นั่งมีความกว้างถึง 56 ซม. เป็นเบาะที่นั่งที่กว้างที่สุด ให้ความรู้สึกสะดวกสบายเวลารับชม ที่นั่งโซนนี้อยู่จุดศูนย์กลางโรงละคร
-               ซึ่งเป็นตำแหน่งที่รับชมการแสดงได้ดีที่สุด  ทำให้คุณได้ชมโชว์ด้วยความประทับใจ จากมุมมองที่ยอดเยี่ยม สามารถรับชมภาพฉายบนจอขนาดใหญ่ที่ฉากหลังและพื้น
-               (High Definition Projection Mapping) ได้อย่างชัดเจนที่สุด และยังเป็นจุดที่รับฟังระบบเสียงที่ได้รับการออกแบบอย่างมีประสิทธิภาพ ได้สมบูรณ์แบบที่สุดด้วย
-            </p>
-            <p>ราคา 4,000 บาท</p>
-            <a target='_blank' href='http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Star'>
-              <button type='button'>BUY TICKET</button>
-            </a>
-          </div>
-          {/* -----------------------------------------------------------------*/}
-          <div className={styles.content} style={this.getStyleForCurrent('cloud')}>
-            <h1>SEATING PLAN</h1>
-            <h3><img src={require('../assets/images/sky-seat.png')} /> CLOUD ZONE</h3>
-            <p>
-              ที่นั่งโซนนี้อยู่บริเวณศูนย์กลางโรงละคร มีมุมมองที่ใกล้เคียงกับ Star Zone เบาะที่นั่งมีความกว้าง 53 ซม. คุณจะได้ชมโชว์ด้วยความประทับใจในมุมมองที่ดี
-              และเต็มอิ่มกับรายละเอียดต่างๆ บนเวที ทั้งระบบแสง เสียง และเทคนิคพิเศษมากมายอย่างเต็มอรรถรส
-            </p>
-            <p>ราคา 3,000 บาท</p>
-            <a target='_blank' href='http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Cloud'>
-              <button type='button'>BUY TICKET</button>
-            </a>
-          </div>
-          {/* -----------------------------------------------------------------*/}
-          <div className={styles.content} style={this.getStyleForCurrent('ocean')}>
-            <h1>SEATING PLAN</h1>
-            <h3><img src={require('../assets/images/ocean-seat.png')} /> OCEAN ZONE</h3>
-            <p>
-            ที่นั่งโซนนี้อยู่ใกล้ชิดเวทีการแสดงมากที่สุด เบาะที่นั่งมีความกว้าง 53 ซม. เหมาะสำหรับผู้ที่ต้องการรับชมการแสดงในระยะใกล้ เห็นรายละเอียดต่างๆ บนเวทีได้อย่างชัดเจน
-            ทำให้คุณได้สัมผัสประสบการณ์การแสดงอย่างใกล้ชิดราวกับได้หลุดเข้าไปในโลกของการแสดงจริงๆ
-            </p>
-            <p>ราคา 3,000 บาท</p>
-            <a target='_blank' href='http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Ocean'>
-              <button type='button'>BUY TICKET</button>
-            </a>
-          </div>
-          {/* -----------------------------------------------------------------*/}
-          <div className={styles.content} style={this.getStyleForCurrent('sky')}>
-            <h1>SEATING PLAN</h1>
-            <h3><img src={require('../assets/images/purple-seat.png')} /> SKY ZONE</h3>
-            <p>
-              ที่นั่งโซนราคาคุ้มค่านี้ อยู่ในจุดรับชมการแสดงที่ถัดออกมาจากโซนอื่นๆ เบาะที่นั่งมีความกว้าง 51 ซม. คุณจะรับชมการแสดงแบบองค์รวมได้อย่างครบอรรถรสที่สุด
-              สามารถมองเห็นรายละเอียดการแสดงที่เกิดขึ้นได้ทั่วทั้งหมด พร้อมกันนี้คุณจะได้ตื่นตาตื่นใจกับฉากหลังสุดอลังการไม่แพ้โซนอื่นๆ เช่นกัน
-            </p>
-            <p>ราคา 2,500 บาท</p>
-            <a target='_blank' href='http://uat-www.pandapass.asia/booking/external/5?ticketClassTypeName=Sky'>
-              <button type='button'>BUY TICKET</button>
-            </a>
           </div>
         </div>
       </div>
